fe68987031789fe751355164a76a67574c203632
Onni Hakala <onni@keksi.io>
2024-01-29 22:09:27 +0100

    Replace with Traveling Van Problem Notion and social links

--- site.config.ts
+++ site.config.ts
@@ -2,29 +2,29 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '708bfd41fda24b989e3f0788dc603f82',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Traveling Van Problem',
+  domain: 'travelingvanproblem.com',
+  author: 'Traveling Van Problem',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'vanproblem',
+  github: 'travelingvanproblem',
+  //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // instagram: '#', // optional instagram username
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  // reddit: '#', // optional reddit username
+  instagram: 'travelingvanproblem', // optional instagram username
+  youtube: 'TravelingVanProblem', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  reddit: 'TravelingVanProblem', // optional reddit username
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -52,16 +52,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
+  navigationStyle: 'custom',
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationLinks: [
+    {
+      title: 'Guidebook',
+      pageId: 'dcf8bd01dfa04a20b2fe93d1cad3d59d'
+    },
+    {
+      title: 'About',
+      pageId: '9f31b4bc3e8e43f095bff906ce1c67ac'
+    }
+  ]
 })
