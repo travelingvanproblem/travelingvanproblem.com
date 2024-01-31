b817b0cd3347a2ffde84b6a63a52f79c54f0bea2
Onni Hakala <onni@keksi.io>
2024-01-31 13:07:42 +0100

    Add logo to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,129 +38,138 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>© Copyright {currentYear}. All rights reserved.</div>
+        <div className={styles.footerSocial}>
+          <div className={styles.settings}>
+            <a href="/" rel="home" title="Logo">
+              <img className={styles.logo} src="/logo.svg"></img>
+            </a>
+          </div>
+          <div className={styles.settings}>
+            {hasMounted && (
+              <a
+                className={styles.toggleDarkMode}
+                href='#'
+                role='button'
+                onClick={onToggleDarkMode}
+                title='Toggle dark mode'
+              >
+                {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+              </a>
+            )}
+          </div>
+          <div className="site-social-links-menu">
+            <div className={styles.social}>
+              {config.twitter && (
+                <a
+                  className={styles.twitter}
+                  href={`https://twitter.com/${config.twitter}`}
+                  title={`Twitter @${config.twitter}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaTwitter />
+                </a>
+              )}
 
-      <div className={styles.settings}>
-        {hasMounted && (
-          <a
-            className={styles.toggleDarkMode}
-            href='#'
-            role='button'
-            onClick={onToggleDarkMode}
-            title='Toggle dark mode'
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        )}
+              {config.mastodon && (
+                <a
+                  className={styles.mastodon}
+                  href={config.mastodon}
+                  title={`Mastodon ${config.getMastodonHandle()}`}
+                  rel='me'
+                >
+                  <FaMastodon />
+                </a>
+              )}
+
+              {config.zhihu && (
+                <a
+                  className={styles.zhihu}
+                  href={`https://zhihu.com/people/${config.zhihu}`}
+                  title={`Zhihu @${config.zhihu}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaZhihu />
+                </a>
+              )}
+
+              {config.github && (
+                <a
+                  className={styles.github}
+                  href={`https://github.com/${config.github}`}
+                  title={`GitHub @${config.github}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaGithub />
+                </a>
+              )}
+
+              {config.linkedin && (
+                <a
+                  className={styles.linkedin}
+                  href={`https://www.linkedin.com/in/${config.linkedin}`}
+                  title={`LinkedIn ${config.author}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaLinkedin />
+                </a>
+              )}
+
+              {config.newsletter && (
+                <a
+                  className={styles.newsletter}
+                  href={`${config.newsletter}`}
+                  title={`Newsletter ${config.author}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaEnvelopeOpenText />
+                </a>
+              )}
+
+              {config.instagram && (
+                <a
+                  className={styles.instagram}
+                  href={`https://www.reddit.com/u/${config.instagram}`}
+                  title={`Instagram ${config.author}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaInstagram />
+                </a>
+              )}
+
+              {config.youtube && (
+                <a
+                  className={styles.youtube}
+                  href={`https://www.youtube.com/${config.youtube}`}
+                  title={`YouTube ${config.author}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaYoutube />
+                </a>
+              )}
+
+              {config.reddit && (
+                <a
+                  className={styles.reddit}
+                  href={`https://www.reddit.com/u/${config.reddit}`}
+                  title={`Reddit ${config.author}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FaReddit />
+                </a>
+              )}
+          </div>
+        </div>
       </div>
-
-      <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
-
-        {config.mastodon && (
-          <a
-            className={styles.mastodon}
-            href={config.mastodon}
-            title={`Mastodon ${config.getMastodonHandle()}`}
-            rel='me'
-          >
-            <FaMastodon />
-          </a>
-        )}
-
-        {config.zhihu && (
-          <a
-            className={styles.zhihu}
-            href={`https://zhihu.com/people/${config.zhihu}`}
-            title={`Zhihu @${config.zhihu}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaZhihu />
-          </a>
-        )}
-
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
-
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
-
-        {config.newsletter && (
-          <a
-            className={styles.newsletter}
-            href={`${config.newsletter}`}
-            title={`Newsletter ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaEnvelopeOpenText />
-          </a>
-        )}
-
-        {config.instagram && (
-          <a
-            className={styles.instagram}
-            href={`https://www.reddit.com/u/${config.instagram}`}
-            title={`Instagram ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaInstagram />
-          </a>
-        )}
-
-        {config.youtube && (
-          <a
-            className={styles.youtube}
-            href={`https://www.youtube.com/${config.youtube}`}
-            title={`YouTube ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaYoutube />
-          </a>
-        )}
-
-        {config.reddit && (
-          <a
-            className={styles.reddit}
-            href={`https://www.reddit.com/u/${config.reddit}`}
-            title={`Reddit ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaReddit />
-          </a>
-        )}
+      <div className="site-info">
+        <div className={styles.copyright}>© Copyright {currentYear}. All rights reserved.</div>
       </div>
     </footer>
   )
